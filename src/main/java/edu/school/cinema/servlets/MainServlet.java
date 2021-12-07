d1a112c7ff287695723690a857dc52cbee1d6b8f
Pa53L <pbeloshein@gmail.com>
2021-12-07 19:57:05 +0300

    Initial commit

---
+++ src/main/java/edu/school/cinema/servlets/MainServlet.java
@@ -0,0 +1,21 @@
+package edu.school.cinema.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/cinema")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello, MR. Anderson!");
+        printWriter.close();
+    }
+}
