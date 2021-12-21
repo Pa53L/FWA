5ddcd80825ec4138fbc6692aa435cc38707056ec
Айрапетян Анна <amarayrapetyan@sberbank.ru>
2021-12-21 12:07:38 +0300

    db population in AppConfig

--- src/main/java/edu/school21/cinema/config/AppConfig.java
+++ src/main/java/edu/school21/cinema/config/AppConfig.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -46,6 +49,12 @@ public class AppConfig {
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(dbUrl, dbUser, dbPassword);
         dataSource.setDriverClassName(driver);
+//        Resource resourceSchema = new ClassPathResource("/sql/schema.sql");
+//        Resource resourceData = new ClassPathResource("/sql/data.sql");
+//        ResourceDatabasePopulator databasePopulatorSchema = new ResourceDatabasePopulator(resourceSchema);
+//        ResourceDatabasePopulator databasePopulatorData = new ResourceDatabasePopulator(resourceData);
+//        databasePopulatorSchema.execute(dataSource);
+//        databasePopulatorData.execute(dataSource);
         return dataSource;
     }
 
