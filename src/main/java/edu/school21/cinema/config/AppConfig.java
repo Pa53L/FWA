a5b483878f8ad764a11f4e3f6cc29c64e2e0e254
Pa53L <pbeloshein@gmail.com>
2022-01-09 09:46:44 +0300

    optimize code partTwo

--- src/main/java/edu/school21/cinema/config/AppConfig.java
+++ src/main/java/edu/school21/cinema/config/AppConfig.java
@@ -1,14 +1,14 @@
 package edu.school21.cinema.config;
 
-import edu.school21.cinema.repositories.UserRepository;
-import edu.school21.cinema.repositories.UserRepositoryImpl;
-import edu.school21.cinema.models.UserRowMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -38,12 +38,12 @@ public class AppConfig {
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(dbUrl, dbUser, dbPassword);
         dataSource.setDriverClassName(driver);
-//        Resource resourceSchema = new ClassPathResource("/sql/schema.sql");
-//        Resource resourceData = new ClassPathResource("/sql/data.sql");
-//        ResourceDatabasePopulator databasePopulatorSchema = new ResourceDatabasePopulator(resourceSchema);
-//        ResourceDatabasePopulator databasePopulatorData = new ResourceDatabasePopulator(resourceData);
-//        databasePopulatorSchema.execute(dataSource);
-//        databasePopulatorData.execute(dataSource);
+        Resource resourceSchema = new ClassPathResource("/sql/schema.sql");
+        Resource resourceData = new ClassPathResource("/sql/data.sql");
+        ResourceDatabasePopulator databasePopulatorSchema = new ResourceDatabasePopulator(resourceSchema);
+        ResourceDatabasePopulator databasePopulatorData = new ResourceDatabasePopulator(resourceData);
+        databasePopulatorSchema.execute(dataSource);
+        databasePopulatorData.execute(dataSource);
         return dataSource;
     }
 
